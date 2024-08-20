141ee5832927ac76320cee69ee4c4d31c287c937
Jack Kenkel <jkenkel@princeton.edu>
2024-08-19 22:05:56 -0600

    local video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,34 @@
 import express from 'express';
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+// middleware - automatically parses Json in request body
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('example');
+
+// recieve video and using ffmpeg convert it to lower quality for posting
+app.post('/process-video', (req, res) => {
+   
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if(!inputFilePath || !outputFilePath) {
+        return res.status(400).send("Missing file path");
+    }
+
+    ffmpeg(inputFilePath).outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+        res.status(200).send("Processing successful");
+    }).on("error", (err) => {
+        console.error(err);
+        res.status(500).send("internal error: " + err.message);
+    }).save(outputFilePath);
 });
 
+// flexibility so port can be set by host
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
